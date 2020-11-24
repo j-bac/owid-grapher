ed6fd66fa4b0277f569da34429c83a94a4f5ac5a
Daniel Gavrilov <dgavrilov95@gmail.com>
2020-11-24 12:45:01 +0000

    Add edge case test: row & column delimiter used in key & value

--- explorer/client/Patch.test.ts
+++ explorer/client/Patch.test.ts
@@ -2,14 +2,31 @@
 
 import {
     DEFAULT_COLUMN_DELIMITER,
+    DEFAULT_ROW_DELIMITER,
+    encodeComponent,
     objectFromPatch,
     objectToPatch,
 } from "./Patch"
 
+const specialCharacters = "!*'();:@&=+$,/?#[]-_.~|\"\\"
+
 it("can create a patch", () => {
     const tests = [
-        { object: { foo: "bar" }, patch: `foo${DEFAULT_COLUMN_DELIMITER}bar` },
         { patch: "", object: {} },
+        { object: { foo: "bar" }, patch: `foo${DEFAULT_COLUMN_DELIMITER}bar` },
+        {
+            object: {
+                [`a${DEFAULT_COLUMN_DELIMITER}${specialCharacters}${DEFAULT_ROW_DELIMITER}c`]: `a${DEFAULT_COLUMN_DELIMITER}${specialCharacters}${DEFAULT_ROW_DELIMITER}b`,
+                [`${DEFAULT_COLUMN_DELIMITER}${DEFAULT_ROW_DELIMITER}`]: specialCharacters,
+            },
+            patch: `${encodeComponent(
+                `a${DEFAULT_COLUMN_DELIMITER}${specialCharacters}${DEFAULT_ROW_DELIMITER}c`
+            )}${DEFAULT_COLUMN_DELIMITER}${encodeComponent(
+                `a${DEFAULT_COLUMN_DELIMITER}${specialCharacters}${DEFAULT_ROW_DELIMITER}b`
+            )}${DEFAULT_ROW_DELIMITER}${encodeComponent(
+                `${DEFAULT_COLUMN_DELIMITER}${DEFAULT_ROW_DELIMITER}`
+            )}${DEFAULT_COLUMN_DELIMITER}${encodeComponent(specialCharacters)}`,
+        },
     ]
     tests.forEach((test) => {
         expect(objectToPatch(test.object)).toEqual(test.patch)
